91a0dac8dc0ee65216e4cb569fbc089b40466883
Michael Gerischer <mi.gerischer@gmail.com>
2026-07-21 10:34:08 +0200

    Initial Commit

---
+++ LiveWallpaper/LiveWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveWallpaperApp.swift
+//  LiveWallpaper
+//
+//  Created by Michael Gerischer on 21.07.26.
+//
+
+import SwiftUI
+
+@main
+struct LiveWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
