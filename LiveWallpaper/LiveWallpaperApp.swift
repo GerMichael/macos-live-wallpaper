25502826e8b4049aff04d4d228e9e26a466d797e
Michael Gerischer <mi.gerischer@gmail.com>
2026-07-21 14:36:56 +0200

    Start with example project

--- LiveWallpaper/LiveWallpaperApp.swift
+++ LiveWallpaper/LiveWallpaperApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LiveWallpaperApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
+            ContentView().windowResizeBehavior(.disabled)
+        }.windowResizability(.contentSize)
     }
 }
